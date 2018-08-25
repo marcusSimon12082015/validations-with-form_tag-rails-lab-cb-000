a722a404fc56136d6d061611a907749753e6687a
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-08-25 18:31:28 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :authors, only: [:show, :new, :create]
+  resources :posts, only: [:show, :update, :edit]
 end
